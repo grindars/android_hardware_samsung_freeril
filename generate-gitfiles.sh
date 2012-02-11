d166ac8c9e540da74a16911fb1cd4ff16cd64b6e
Sergey Gridassov <grindars@gmail.com>
2012-02-11 11:57:13 +0400

    Build system tweaks.

--- generate-gitfiles.sh
+++ generate-gitfiles.sh
@@ -26,14 +26,9 @@ distdir="$2"
 # check for git
 which git >/dev/null 2>&1 || exit 0
 
-if [ `wc -l "${distdir}/ChangeLog" | cut -d ' ' -f 1` -eq 0 ]; then
-	echo "Generating ChangeLog"
+echo "Generating ChangeLog"
+git log --date=iso8601 --format="%ad  %cN <%cE>%n%n%w(72,4)%B%n" > ${distdir}/ChangeLog
 
-	git log --date=iso8601 --format="%ad  %cN <%cE>%n%n%w(72,4)%B%n" > ${distdir}/ChangeLog
-fi
+echo "Generating AUTHORS"
+git log --format='%cN <%cE>' | sort -u > ${distdir}/AUTHORS
 
-if [ `wc -l "${distdir}/AUTHORS" | cut -d ' ' -f 1` -eq 0 ]; then
-	echo "Generating AUTHORS"
-
-	git log --format='%cN <%cE>' | sort -u > ${distdir}/AUTHORS
-fi
